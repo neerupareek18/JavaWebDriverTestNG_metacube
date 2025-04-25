4e7ce23aba665a3a89176fbe5e08c4013327d170
Neeru Pareek <neerupareek18@gmail.com>
2025-04-25 13:20:16 +0530

    Removed Error

--- src/test/java/Assignment1.java
+++ src/test/java/Assignment1.java
@@ -1,15 +1,18 @@
+import org.testng.annotations.Test;
+
 public class Assignment1 {
 
+    @Test
     public void testCase(){
         System.out.println("Learning JavaWebDriver TestNG");
     }
 }
-    /*1. Install JDK -- Done
-2. Install Eclipse -- Installed IntelliJ
-3. Create Maven Project -- Done
-4. Add WebDriver Dependencies to your maven project using POM.xml -- Done
-5. Configure testng for Eclipse -- Done in IntelliJ
-6. Add TestNg Dependencies to your maven project using POM.xml -- Done
-7. Convert your maven project to testng Project -- Done
-8. Add a class in above project and write any simple script. -- Done/*
+//    1. Install JDK -- Done
+//2. Install Eclipse -- Installed IntelliJ
+//3. Create Maven Project -- Done
+//4. Add WebDriver Dependencies to your maven project using POM.xml -- Done
+//5. Configure testng for Eclipse -- Done in IntelliJ
+//6. Add TestNg Dependencies to your maven project using POM.xml -- Done
+//7. Convert your maven project to testng Project -- Done
+//8. Add a class in above project and write any simple script. -- Done
 
